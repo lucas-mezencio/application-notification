e2c0901ab51f86252d7d68e7599a9a1c9a0d34de
Lucas Mezencio <lucasmezencio@outlook.com>
2021-08-11 11:39:18 -0300

    create local notification method

--- app/src/main/java/com/lucasmezencio/appnotification/MainActivity.kt
+++ app/src/main/java/com/lucasmezencio/appnotification/MainActivity.kt
@@ -2,10 +2,19 @@ package com.lucasmezencio.appnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener {
+            this.showNotification("1234", "Bootcamp Android", "Curso Kotlin Android")
+        }
     }
 }
